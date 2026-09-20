8dc40a5ecd7eeedbcca3d62d5abf14227a6a2571
HitBoxHero <68302323+HitboxHero@users.noreply.github.com>
2026-09-20 12:49:48 -0700

    Decompile countdown NPC clip callbacks

--- src/overlays/actors/ovl_Countdown_Npc0/ac_countdown_npc0.c
+++ src/overlays/actors/ovl_Countdown_Npc0/ac_countdown_npc0.c
@@ -27,11 +27,17 @@ ActorProfile Countdown_Npc0_Profile = {
 
 #pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/actors/ovl_Countdown_Npc0/ac_countdown_npc0/aCD0_actor_ct.s")
 
-#pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/actors/ovl_Countdown_Npc0/ac_countdown_npc0/aCD0_actor_save.s")
+void aCD0_actor_save(Actor* thisx, Game_Play* game_play) {
+    common_data.clip.unk_040->unk_C8(thisx, game_play);
+}
 
-#pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/actors/ovl_Countdown_Npc0/ac_countdown_npc0/aCD0_actor_dt.s")
+void aCD0_actor_dt(Actor* thisx, Game_Play* game_play) {
+    common_data.clip.unk_040->unk_C4(thisx, game_play);
+}
 
-#pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/actors/ovl_Countdown_Npc0/ac_countdown_npc0/aCD0_actor_init.s")
+void aCD0_actor_init(Actor* thisx, Game_Play* game_play) {
+    common_data.clip.unk_040->unk_CC(thisx, game_play);
+}
 
 #pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/actors/ovl_Countdown_Npc0/ac_countdown_npc0/func_809E0C04_jp.s")
 
--- src/overlays/actors/ovl_Countdown_Npc1/ac_countdown_npc1.c
+++ src/overlays/actors/ovl_Countdown_Npc1/ac_countdown_npc1.c
@@ -27,9 +27,13 @@ ActorProfile Countdown_Npc1_Profile = {
 
 #pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/actors/ovl_Countdown_Npc1/ac_countdown_npc1/aCD1_actor_ct.s")
 
-#pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/actors/ovl_Countdown_Npc1/ac_countdown_npc1/aCD1_actor_save.s")
+void aCD1_actor_save(Actor* thisx, Game_Play* game_play) {
+    common_data.clip.unk_040->unk_C8(thisx, game_play);
+}
 
-#pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/actors/ovl_Countdown_Npc1/ac_countdown_npc1/aCD1_actor_dt.s")
+void aCD1_actor_dt(Actor* thisx, Game_Play* game_play) {
+    common_data.clip.unk_040->unk_C4(thisx, game_play);
+}
 
 #pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/actors/ovl_Countdown_Npc1/ac_countdown_npc1/aCD1_actor_init.s")
 
