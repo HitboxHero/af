fbdc833d6f74b7f5958a293483801a3c8e3d6730
HitBoxHero <68302323+HitboxHero@users.noreply.github.com>
2026-09-25 10:07:06 -0700

    m_titledemo batch 2 (4/4 matched)

--- src/code/m_titledemo.c
+++ src/code/m_titledemo.c
@@ -1,11 +1,35 @@
 #include "global.h"
 #include "6EC9E0.h"
+#include "overlays/gamestates/ovl_play/m_play.h"
+#include "m_player_lib.h"
+#include "m_bgm.h"
+#include "m_lib.h"
+#include "m_event.h"
 
-#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_titledemo/func_800C8D40_jp.s")
+u16 func_800C8D40_jp(s32 titledemo_no, s32 key) {
+    extern void* B_80144690_jp;
+    extern u16* D_8010EDA8_jp[];
+
+    SegmentBaseAddress[6] = (uintptr_t)B_80144690_jp - K0BASE;
+    return ((u16*)Lib_SegmentedToVirtual(D_8010EDA8_jp[titledemo_no]))[key];
+}
 
 #pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_titledemo/func_800C8D98_jp.s")
 
-#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_titledemo/mTD_demono_get.s")
+UNK_RET mTD_demono_get(void) {
+    extern s32 D_8010EDA4_jp;
+
+    if (D_8010EDA4_jp == -1) {
+        D_8010EDA4_jp = 1;
+    } else {
+        D_8010EDA4_jp++;
+        if (D_8010EDA4_jp > 5) {
+            D_8010EDA4_jp = 1;
+        }
+    }
+
+    return D_8010EDA4_jp;
+}
 
 #pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_titledemo/mTD_player_keydata_init.s")
 
@@ -18,9 +42,24 @@ void func_800C8F5C_jp(void) {
     func_800B5980_jp(func_800C8ECC_jp());
 }
 
-#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_titledemo/func_800C8F84_jp.s")
+void func_800C8F84_jp(Game_Play* game_play) {
+    game_play->unk_1EE0 = 10;
+    game_play->unk_1EE1 = 3;
+    mPlib_request_main_invade_type1(game_play);
+    mBGMPsComp_make_ps_wipe(0x521C);
+}
 
-#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_titledemo/title_demo_move.s")
+void title_demo_move(Game_Play* game_play) {
+    extern s32 D_8010EDA0_jp;
+
+    if (mEv_CheckTitleDemo() > 0) {
+        func_800C8F5C_jp();
+        D_8010EDA0_jp++;
+        if (D_8010EDA0_jp >= 1800) {
+            func_800C8F84_jp(game_play);
+        }
+    }
+}
 
 #pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_titledemo/func_800C9010_jp.s")
 
